16af7995939c9941e1912653dc35f7d426338146
root <root@pyronia.net>
2015-05-25 01:03:57 -0400

    inital commit

---
+++ library/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * bootstrap.php
+ *
+ * Booter for the MVC
+ * 
+ * long desc
+ *
+ * @package    PackageName
+ * @author     Phillip Carr
+ * @copyright  2015 Phillip
+ * @version    CVS: $Id:$
+ */
+
+require_once (ROOT . DS . 'config' . DS . 'config.php');
+require_once (ROOT . DS . 'library' . DS . 'shared.php');
